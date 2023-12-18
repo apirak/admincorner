d7441641fcc94d2e1b4ccdb9f1f230f44ed2dc06
Apirak Panatkool <apirakb@gmail.com>
2023-12-19 06:37:18 +0700

    Add member index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :members
-  resources :teams
+  resources :teams do
+    resources :members do
+      collection { post :invite }
+    end
+  end
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
   devise_for :users
+
   get "dashboard", to: "pages#dashboard"
   get "landing_page", to: "pages#landing_page"
-  root "pages#landing_page"
+  # root "pages#landing_page"
 end
