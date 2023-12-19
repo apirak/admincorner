b58f99953d741a96f331246ac29c680105db5eb2
Apirak Panatkool <apirakb@gmail.com>
2023-12-20 06:09:24 +0700

    Set root to landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
 
   devise_for :users
 
   get "dashboard", to: "pages#dashboard"
   get "landing_page", to: "pages#landing_page"
-  # root "pages#landing_page"
+  root "pages#landing_page"
 end
