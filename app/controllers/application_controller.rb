da5064a68611330d924627f7df06c11b53c4fcd2
Apirak Panatkool <apirakb@gmail.com>
2024-01-05 21:26:45 +0700

    don't set team for devise controller for new and create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_current_team
-
   def after_sign_in_path_for(resource)
     dashboard_path # replace with your actual dashboard path
   end
@@ -9,10 +8,12 @@ class ApplicationController < ActionController::Base
   private
 
   def set_current_team
-    logger.debug "params: #{params}"
-    @current_team = Team.find(params[:team_id]) if params[:team_id]
-    logger.debug "Current team1: #{@current_team}"
-    @current_team = current_user.teams.first unless @current_team
-    logger.debug "Current team2: #{@current_team}"
+    unless devise_controller? && %i[new create].include?(action_name)
+      if params[:team_id]
+        @current_team = Team.find(params[:team_id])
+      elsif user_signed_in?
+        @current_team ||= current_user.teams.take
+      end
+    end
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class PagesController < AuthorizedController
   skip_before_action :authenticate_user!, only: :landing_page
 
